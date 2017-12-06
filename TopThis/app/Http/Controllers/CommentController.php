421292aa6fefb72223c3585cbb4e59aec3fbd0a4
X <33039497+x@users.noreply.github.com>
2017-12-06 17:01:42 +0100

    ups

--- TopThis/app/Http/Controllers/CommentController.php
+++ TopThis/app/Http/Controllers/CommentController.php
@@ -50,16 +50,14 @@ class CommentController extends Controller {
         } else {
             return redirect()->back()->with('message', 'You need to login');
         }
-
         $comment = new Comment();
         $comment->replay_id = NULL;
         $comment->user_id = $u_id;
         $comment->body = $request->body;
         $comment->top()->associate($top);
-
+        $comment->approuved=1;//1 - needs to be approuved by admin/moderator
         $user = User::find($comment->user_id);
         $comment->user()->associate($user);
-
         $comment->save();
         return redirect()->back()->with('message', 'Comment added');
     }
@@ -122,14 +120,47 @@ class CommentController extends Controller {
      * @param  \App\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Comment $comment) {
+    public function deletecomment(Request $request) {
         //
+        $request->all();
+        $temp=  $request->commentid;
+         $comment=Comment::where('id',$temp)->first();
+        
+     //   $comment=Comment::where($temp,'id')->get();
+        $comment->approuved=2;
+        $comment->save();
+         
+        return response()->json(['message' => $comment->id]);      
     }
-    public function incrementvote(Request $comment){
+    
+    public function incrementvote(Request $request){
         
-        $temp=Comment::find($comment);
-        $temp->up_vote=1;
+        $request->all();
+        $commentid =  $request->commentid;
+        $temp=Comment::where('id',$commentid)->first();
+        if( $temp->up_vote === NULL){
+            $temp->up_vote=1;         
+        }else{
+        $temp->increment('up_vote');
+        }
+        $temp->save();
         
-        return response()->json(['status' => 'success'], 201);
+    //    return response()->json(['status' => 'success'], 201);
+    return response()->json(['message' => $temp->id]);
+    }
+        public function decrementvote(Request $request){
+        
+        $request->all();
+        $commentid =  $request->commentid;
+        $temp=Comment::where('id',$commentid)->first();
+        if( $temp->down_vote === NULL ){
+            $temp->down_vote=1;         
+        }else{
+        $temp->increment('down_vote');
+        }
+        $temp->save();
+        
+    //    return response()->json(['status' => 'success'], 201);
+    return response()->json(['message' => $temp->id]);
     }
 }
