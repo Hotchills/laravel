9be2df604720aa5ee4fed8dbf814eb75f8e92d44
X <33039497+x@users.noreply.github.com>
2018-05-28 16:58:16 +0200

    9

---
+++ TipTop/app/LikeTop.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LikeTop extends Model
+{
+    //
+        public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+        public function top()
+    {
+        return $this->belongsTo('App\Top');
+    }
+        public function upvotestop()
+    {
+         $likes=LikeTop::where('top_id',  $this->top_id)->where('liketop','1')->count();
+        return $likes;
+    }
+        public function downvotestop()
+    {
+        $dislikes=LikeTop::where('top_id', $this->top_id)->where('liketop','2')->count();
+        return $dislikes;
+    }
+}
