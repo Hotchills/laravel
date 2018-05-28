8f273e8eeb8be8eb34073715a1204cc6f50dc1b8
X <33039497+x@users.noreply.github.com>
2018-05-28 16:58:12 +0200

    8

---
+++ TipTop/app/Like.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+        public function comment()
+    {
+        return $this->belongsTo('App\Comment');
+    }
+    public function upvotescomment()
+    {
+         $likes=Like::where('comment_id',  $this->comment_id)->where('like','1')->count();
+        return $likes;
+    }
+        public function downvotescomment()
+    {
+        $dislikes=Like::where('comment_id', $this->comment_id)->where('like','2')->count();
+        return $dislikes;
+    }
+}
